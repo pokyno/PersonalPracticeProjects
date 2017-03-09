4ba0b1c2aa1c28a4d5c862829c6e906eca3f73ab
Pieter van Berkel <pokyno96@gmail.com>
2017-03-09 18:10:06 +0100

    The main menu of the program

---
+++ APL.py
@@ -0,0 +1,34 @@
+import io
+programs = [
+
+]
+
+def init():
+    print("------------------------------------------")
+    print("The programs that currently installed are:\n")
+    print("------------------------------------------")
+    counter = 0
+    for program in programs:
+        print("%d- %s\n" % (counter,program.name))
+        counter += 1
+    print("------------------------------------------")
+
+def main():
+    init()
+    while(True):
+        print("Enter the desired program number(-1 to exit):")
+        userInput = 0
+        try:
+            userInput = int(input())
+        except:
+            print("Please enter a existing option")
+            continue
+
+        if userInput == -1: #user wants to exit the program
+            print("Program closing")
+            break
+
+        if 0 <= userInput and userInput < programs.count:
+            programs[userInput].start() #start the application
+
+main()
